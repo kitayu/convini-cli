3d69ed6afcf78dc080ec1f2149ce120cba65061e
Yoshihisa Kitayuguchi <kippo14@gmail.com>
2022-02-23 11:38:56 +0900

    バージョン情報をビルド時に埋め込めるように修正

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"runtime/debug"
 )
+
+// バージョン
+var version = ""
 
 func main() {
 
@@ -14,6 +18,24 @@ func main() {
 	flag.Parse()
 
 	if v {
-		fmt.Println("convini-cli version 1.0.0")
+		fmt.Printf("convini-cli %s`\n", getVersion())
 	}
+}
+
+/*
+ * バージョン情報を取得します。
+ * 実行時に以下のオプションを設定することでバージョンが表示されます。
+ * go build -ldflags '-s -w -X main.version={表示するバージョンを指定}'
+ */
+func getVersion() string {
+	if version != "" {
+		// バージョン情報が設定されている場合
+		return version
+	}
+
+	i, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+	return i.Main.Version
 }
